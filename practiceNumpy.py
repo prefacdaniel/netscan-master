f13e4a8f6c728dc04c34a783fffe448f1738da82
dprefac <prefacdaniel@gmail.com>
2019-04-27 00:18:10 +0300

    -start k-mean

--- practiceNumpy.py
+++ practiceNumpy.py
@@ -2,6 +2,12 @@ import numpy as np
 from scipy import stats
 
 
+def normalized(a, axis=-1, order=2):
+    l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
+    l2[l2 == 0] = 1
+    return a / np.expand_dims(l2, axis)
+
+
 def eliminate_outlier_column(data, column, m=2):  # smaller the m, the sensible became
     mean = np.mean(data, axis=0)
     sd = np.std(data, axis=0)
@@ -10,15 +16,16 @@ def eliminate_outlier_column(data, column, m=2):  # smaller the m, the sensible 
     return np.array([x for x in data if (abs(x[column] - mean[column]) < m * sd[column]).any()])
 
 
+def normalise_data_set(data_set):
+    return np.array((data_set - data_set.mean(axis=0)) / data_set.std(axis=0))
+
 data = np.array([[600, 700, 4],
                  [500, 504, 5],
                  [600, 400, 6],
                  [1, 500, 5],
                  [500, 1, 3],
-                 [600, 560, 4]])
-
+                 [600, 560, 1]])
 print("Initial print:")
-print(data)
 # data = np.array([x for x in data if (abs(x - np.mean(data)) < 2 * np.std(data)).all()]) #for all
 
 # mean = np.mean(data, axis=0)
@@ -30,12 +37,18 @@ print(data)
 # data = np.array([x for x in data if (abs(x[0] - mean[0]) < 2 * sd[0]).all()]) #eliminare outlier pt o coloana
 # data = np.array([x for x in data if (abs(x - mean) < 2 * sd).all()])#eliminare outlier pt tot arrayu
 
+print(data)
+
 data = eliminate_outlier_column(data, 1)
-
-print("After eliminating outlayers form col 0:")
-print(data)
-
 data = eliminate_outlier_column(data, 0)
+# data = normalized(data, 0)
+# print(data)
 
-print("After eliminating outlayers form col 1:")
 print(data)
+
+
+normed = normalise_data_set(data)
+print(normed)
+
+print(normed.std(axis=0))
+print(normed.mean(axis=0))
--- readCsv.py
+++ readCsv.py
@@ -7,7 +7,17 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
-def eliminate_outlier_column(data, column, m=2):  # smaller the m, the sensible became
+def normalise_training_data_set(data_set, data_set_mean, data_set_std):
+    return np.array((data_set - data_set_mean) / data_set_std)
+
+
+def normalise_data_set(data_set):
+    data_set_mean = data_set.mean(axis=0)
+    data_set_std = data_set.std(axis=0)
+    return np.array((data_set - data_set_mean) / data_set_std), data_set_mean, data_set_std
+
+
+def eliminate_outlier_column(data, column, m=3):  # smaller the m, the sensible became
     mean = np.mean(data, axis=0)
     sd = np.std(data, axis=0)
     return np.array([x for x in data if (abs(x[column] - mean[column]) < m * sd[column]).any()])
@@ -25,12 +35,10 @@ def load_csv_and_extract_feature(fileName, indexs, label):
         return data
 
 
-filePathRusia = 'C:\\Users\\dprefac\\PycharmProjects\\KMeans\\csv\\pythonScript_1000_telekom_VPNRusia.csv'
-filePathLocal = 'C:\\Users\\dprefac\\PycharmProjects\\KMeans\\csv\\bruteForceWithValidPassword100Attempts200Status.csv'
+filePathRusia = 'C:\\Users\\dprefac\\PycharmProjects\\netscan-master\\csv\\pythonScript_1000_telekom_VPNRusia.csv'
+filePathLocal = 'C:\\Users\\dprefac\\PycharmProjects\\netscan-master\\csv\\bruteForceWithValidPassword100Attempts200Status.csv'
 
-
-
-features = [9, 10, 12, 13, 14]
+features = [9, 10, 13, 14]
 
 dataFromRusia = load_csv_and_extract_feature(filePathRusia, features, "RUSIA")
 dataFromLocal = load_csv_and_extract_feature(filePathLocal, features, "LOCAL")
@@ -44,15 +52,33 @@ dataFromLocal = eliminate_outlier_column(dataFromLocal, 0)
 dataFromRusia = eliminate_outlier_column(dataFromRusia, 1)
 dataFromLocal = eliminate_outlier_column(dataFromLocal, 1)
 
+kMeanRusia = np.copy(dataFromRusia)
+kMeanLocal = np.copy(dataFromLocal)
+
+print(dataFromLocal.mean(axis=0))
+print(dataFromLocal.std(axis=0))
+print("old local")
+print(dataFromLocal)
+dataFromLocal, mean, std = normalise_data_set(dataFromLocal)
+print("new local")
+print(dataFromLocal)
+print(dataFromLocal.mean(axis=0))
+print(dataFromLocal.std(axis=0))
+
+print("old rusia data")
+print(dataFromRusia)
+dataFromRusia = normalise_training_data_set(dataFromRusia, mean, std)
+print("new rusia data")
 print(dataFromRusia)
 
+# dataFromRusia.concatenate(dataFromLocal)
 
 fig = plt.figure(1, figsize=(8, 8))
 plt.clf()
 ax = Axes3D(fig, rect=[0, 0, 1, 1], elev=8, azim=200)
 plt.cla()
-ax.scatter(dataFromLocal[:, 0], dataFromLocal[:, 1], dataFromLocal[:, 4], c="B")
-ax.scatter(dataFromRusia[:, 0], dataFromRusia[:, 1], dataFromRusia[:, 4], c="R")
+ax.scatter(dataFromLocal[:, 0], dataFromLocal[:, 1], dataFromLocal[:, 3], c="B")
+ax.scatter(dataFromRusia[:, 0], dataFromRusia[:, 1], dataFromRusia[:, 3], c="R")
 ax.w_xaxis.set_ticklabels([])
 ax.w_yaxis.set_ticklabels([])
 ax.w_zaxis.set_ticklabels([])
@@ -60,3 +86,12 @@ ax.set_xlabel('Petal width')
 ax.set_ylabel('Sepal length')
 ax.set_zlabel('Petal length')
 plt.show()
+
+print("k-mean")
+print("k-local")
+print(kMeanLocal)
+print("k-rusia")
+print(kMeanRusia)
+k_data = numpy.concatenate([kMeanRusia, kMeanLocal])
+print("k-data")
+print(k_data)
