f4a399db12b8c0c7165104fcfa87227629043e14
dprefac <prefacdaniel@gmail.com>
2019-05-06 19:57:29 +0300

    traffic capture

--- sniffer.py
+++ sniffer.py
@@ -1,5 +1,7 @@
-import pyshark
+import queue
+import threading
 
+import pyshark
 
 # cap = pyshark.FileCapture('C:\\Users\\dprefac\\PycharmProjects\\KMeans\\wiresharkScans\\tryaspcap.cap')
 
@@ -10,18 +12,49 @@ import pyshark
 # for packet in capture.sniff_continuously(packet_count=5):
 #     print(packet)
 #
-def packet_captured(packet):
+
+
+packet_queue = queue.Queue()
+
+
+def extract_data(stream):
+    pass #todo
+    # capture_time = packet.frame_info.time_epoch  # epoch time when this frame (packet) was captured
+    # source_ip = packet.ip.src  # the source IP of this packet
+    # stream_index = packet.tcp.stream  # a sintetic unique identifier (autoincremntal integer) gave by tshark to each TCP stream
+    # time_delta = packet.tcp.time_delta  # Time since previous frame in this TCP stream
+    # time_relative = packet.tcp.time_relative  # Time since first frame in this TCP stream
+    # tcp_payload_size = packet.tcp.len  # tpc payload size in bytes
+    # print(capture_time)
+
+
+def extract_stream():
+    pass  # todo extract a stream by some rules (timeout or socket close) and after that, call extract_data method
+
+
+stream_exaction_thread = threading.Thread(target=extract_stream)
+stream_exaction_thread.setDaemon(True)
+stream_exaction_thread.start()
+
+
+def sort_and_filter_by_stream_index(packet):
+    pass  # todo skip already existent connexion packets
+
+
+def filter_packets(packet):
     if packet["ip"].proto == "6":
-        print("\n\n####################")
-        print('Just arrived:', packet)
-        print("\n\n####################")
-    else:
-        print("no")
+        sort_and_filter_by_stream_index(packet)
 
 
-capture = pyshark.LiveCapture(bpf_filter="tcp")
+def capture_traffic(packet):
+    t = threading.Thread(target=filter_packets(packet))
+    t.setDaemon(True)
+    t.start()
+
+
+capture = pyshark.LiveCapture()
 # capture.sniff_continuously()
-capture.apply_on_packets(packet_captured)
+capture.apply_on_packets(capture_traffic)
 
 # print(cap[1])
 # print(cap)
