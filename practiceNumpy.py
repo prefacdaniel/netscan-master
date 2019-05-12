feb116201bbc828560fccc8142b15932a0cb9fc7
dprefac <prefacdaniel@gmail.com>
2019-05-12 23:54:03 +0300

    eliminate outlayer

--- practiceNumpy.py
+++ practiceNumpy.py
@@ -16,39 +16,77 @@ def eliminate_outlier_column(data, column, m=2):  # smaller the m, the sensible 
     return np.array([x for x in data if (abs(x[column] - mean[column]) < m * sd[column]).any()])
 
 
+def eliminate_outlier(data, m=2):
+    return data[abs(data - np.mean(data)) < m * np.std(data)]
+
+
+def reject_outliers(data, m=2.):
+    d = np.abs(data - np.median(data))
+    mdev = np.median(d)
+    s = d / mdev if mdev else 0.
+    return data[s < m]
+
+
 def normalise_data_set(data_set):
     return np.array((data_set - data_set.mean(axis=0)) / data_set.std(axis=0))
 
-data = np.array([[600, 700, 4],
-                 [500, 504, 5],
-                 [600, 400, 6],
-                 [1, 500, 5],
-                 [500, 1, 3],
-                 [600, 560, 1]])
-print("Initial print:")
-# data = np.array([x for x in data if (abs(x - np.mean(data)) < 2 * np.std(data)).all()]) #for all
 
-# mean = np.mean(data, axis=0)
-# print("mean is: ", mean)
-## print("sd is: ", sd)
-# sd = np.std(data, axis=0)
-# print("sd is: ", sd)
+def detect_outlier(data_1, m=2):
+    outliers = []
+    threshold = m
+    mean_1 = np.mean(data_1, axis=0)
+    std_1 = np.std(data_1, axis=0)
+
+    for y in data_1:
+        z_score = (y - mean_1) / std_1
+        if (np.abs(z_score) > threshold).any():
+            outliers.append(y)
+    return outliers
+
+
+def eliminate_outlier_with_z_score(data_1, m=2):
+    data = []
+    threshold = m
+    mean_1 = np.mean(data_1, axis=0)
+    std_1 = np.std(data_1, axis=0)
+
+    for y in data_1:
+        z_score = (y - mean_1) / std_1
+        if not (np.abs(z_score) > threshold).any():
+            data.append(y)
+    return data
+
 #
-# data = np.array([x for x in data if (abs(x[0] - mean[0]) < 2 * sd[0]).all()]) #eliminare outlier pt o coloana
-# data = np.array([x for x in data if (abs(x - mean) < 2 * sd).all()])#eliminare outlier pt tot arrayu
-
-print(data)
-
-data = eliminate_outlier_column(data, 1)
-data = eliminate_outlier_column(data, 0)
-# data = normalized(data, 0)
+# data = np.array([[600, 700, 4],
+#                  [500, 504, 5],
+#                  [600, 400, 6],
+#                  [1, 500, 5],
+#                  [500, 1, 3],
+#                  [600, 560, 1]])
+# print("Initial print:")
+# # data = np.array([x for x in data if (abs(x - np.mean(data)) < 2 * np.std(data)).all()]) #for all
+#
+# # mean = np.mean(data, axis=0)
+# # print("mean is: ", mean)
+# ## print("sd is: ", sd)
+# # sd = np.std(data, axis=0)
+# # print("sd is: ", sd)
+# #
+# # data = np.array([x for x in data if (abs(x[0] - mean[0]) < 2 * sd[0]).all()]) #eliminare outlier pt o coloana
+# # data = np.array([x for x in data if (abs(x - mean) < 2 * sd).all()])#eliminare outlier pt tot arrayu
+#
 # print(data)
-
-print(data)
-
-
-normed = normalise_data_set(data)
-print(normed)
-
-print(normed.std(axis=0))
-print(normed.mean(axis=0))
+#
+# data = eliminate_outlier_column(data, 1)
+# data = eliminate_outlier_column(data, 0)
+# # data = normalized(data, 0)
+# # print(data)
+#
+# print(data)
+#
+#
+# normed = normalise_data_set(data)
+# print(normed)
+#
+# print(normed.std(axis=0))
+# print(normed.mean(axis=0))
--- train_model.py
+++ train_model.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 from database_connection import select_all_data_from_feature
+from practiceNumpy import eliminate_outlier_column, reject_outliers, eliminate_outlier, detect_outlier, \
+    eliminate_outlier_with_z_score
 
 
 def new_training(model_id, feature_vectors):
@@ -11,10 +13,10 @@ telekom_data = select_all_data_from_feature("pythonScript_1000_vpn_telekom_valid
 
 # for t in telekom_data:
 #     print(t)
-training_data = np.array(telekom_data)#converting to numpy
+training_data = np.array(telekom_data)  # converting to numpy
 print("numpy data: ")
 print(training_data[0])
-training_data = training_data[:, [1, 2, 3, 4, 5, 6, 8]]#extracting only necessary columns
+training_data = training_data[:, [1, 2, 3, 4, 5, 6, 8]]  # extracting only necessary columns
 print("remaining numpy data: ")
 print(training_data[0])
 training_data = np.array(training_data).astype(np.float64)
@@ -24,3 +26,7 @@ print("mean: ")
 print(np.mean(training_data, axis=0))
 print("std: ")
 print(np.std(training_data, axis=0))
+print("initial size", len(training_data))
+training_data = eliminate_outlier_with_z_score(training_data, m=2)
+print("size after` eliminating outliers", len(training_data))
+
