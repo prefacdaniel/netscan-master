279f0645a7aeadf4c6b0570fd9996efe97e5bda9
dprefac <prefacdaniel@gmail.com>
2019-05-12 20:54:19 +0300

    extract data from db

--- database_connection.py
+++ database_connection.py
@@ -33,3 +33,30 @@ def insert_data(feature_vectors, instance_name):
                            )])
     conn.commit()
     conn.close()
+
+
+def select_columns_data_from_feature(instance_name, column_list):
+    conn = sqlite3.connect('C:\\Users\\dprefac\\Documents\\disertatie\\sqlite\\databases\\feature_vectors.db')
+    cursor = conn.cursor()
+    sql = 'SELECT '
+    for column in column_list:
+        sql = sql + column + " ,"
+    sql = sql[:-1]
+    sql = sql + 'FROM feature where instance_name LIKE \"' + instance_name + '\";'
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    return rows
+
+
+def select_all_data_from_feature(instance_name):
+    conn = sqlite3.connect('C:\\Users\\dprefac\\Documents\\disertatie\\sqlite\\databases\\feature_vectors.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM feature where instance_name LIKE \"" + instance_name + "\"")
+    rows = cursor.fetchall()
+    return rows
+
+
+feature_vectors = select_columns_data_from_feature("hydra_1000_vpn_rusia1.pcapng", ['irtt', 'total_time', 'time_value_sin', 'time_value_cos', 'data_len'])
+
+for feature in feature_vectors:
+    print(feature)
